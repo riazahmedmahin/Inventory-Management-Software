c79baf28c7623f3d51de9aa0bba6bf3363a6f3b9
Riaz Ahmed Mahin <riazahmedmahin@gmail.com>
2021-11-29 18:18:25 +0600

    from

---
+++ src/simpleproject/Simpleproject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simpleproject;
+
+/**
+ *
+ * @author Azam corporation
+ */
+public class Simpleproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
